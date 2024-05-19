e588bbf85fec061d286f85094c4e4e4a35953117
Jean-Pierre Tanguay <jeanpiertanguay@gmail.com>
2024-05-19 15:42:47 -0400

    commenté. Restructuré

--- app.py
+++ app.py
@@ -6,24 +6,18 @@
 #
 # 
 #
-import plotly.express as px
-
-import streamlit as st
-import pandas as pd
-import seaborn as sns
 
 
-import matplotlib.pyplot as plt
 import numpy as np
-
-from sklearn.preprocessing import StandardScaler
-from sklearn.pipeline import make_pipeline
-from sklearn.cluster import KMeans
+import pandas as pd
+import matplotlib.pyplot as plt
 from scipy.fft import fft, ifft, fftfreq 
 
-import os
 
-#st.write(os.getcwd())
+import plotly.express as px
+import streamlit as st
+
+
 
 st.set_page_config(
     page_title="Feux de forêts", 
@@ -49,35 +43,47 @@ df = load_data()
 
 colnames = df.columns.to_list()
 
+jurisdictions = np.sort(df.jurisdiction.unique())
+origins = np.sort(df.origin.unique()) 
 
 
 #######################################
-#  display
+# sidebar
 #######################################
+jur_list = ["Tout"]
+jur_list.extend(jurisdictions)
+#jur_list
 
+
+sb = st.sidebar
+sb.title("Juridictions")
+sb.header("Provinces et territoires")
+jur = sb.radio("Sélectionnez la juridiction: ", options=jur_list, horizontal = True)
+
+
+
+#######################################
+#  header
+#######################################
 
 st.title("Feux de forêts - Canada - 1990 à 2021")
-
-
-
 
 #col2.image("countries.png")
 st.write(
     """
-    Environnement Canada
+    Source: La Base nationale de données sur les feux de forêt du Canada (BNDFFC) 
     
-    Les superficies sont en hectares
+    :heavy_exclamation_mark: Note: Les superficies sont en hectares
     """
     
 )
 
 
-
+#######################################
+#  données source
+#######################################
 
 st.header("Données source")
-
-jurisdictions = np.sort(df.jurisdiction.unique())
-origins = np.sort(df.origin.unique()) 
 
 with st.container(border=True):
 
@@ -93,7 +99,7 @@ with st.container(border=True):
             "<a href='/' target='_blank'>Data source</a>",
             unsafe_allow_html=True
         )   
-    #with st.expander("Statistiques"):
+
         
     with tab2:
         col1, col2 = st.columns([3,4] )
@@ -120,52 +126,42 @@ with st.container(border=True):
         
  
 
+#
 ############################################
 # superficie
 ############################################ 
        
+# pas utilisé
 date_start = int(df["year"].min())
 date_end = int(df["year"].max())
-
 #date_start
 #date_end
-    
+
+
+
+
 df_2 = df
-#df_2
-
-##########################################################
-# sidebar
-##########################
-jur_list = ["Tout"]
-jur_list.extend(jurisdictions)
-#jur_list
-
-
-sb = st.sidebar
-sb.title("Juridictions")
-sb.header("Provinces et territoires")
-jur = sb.radio("Sélectionnez la juridiction: ", options=jur_list, horizontal = True)
-#
-
-#mask
-
 if (jur == "Tout"):
     df_jur = df_2
 else:    
     mask = (df_2["jurisdiction"] == jur)
     df_jur = df_2.loc[mask]
     
-
 #df_jur.describe
 df_jur_no_origin = df_jur.groupby([ 'year']).agg({'area': 'sum'})
 df_jur_no_origin = df_jur_no_origin.reset_index()
 
+
 with st.container(border=True):
+
     st.subheader("Superficie brulée par année"  + " (" + jur + ")")
-    st.write("Sélectionnez la juridiction dans le panneau latéral")
+    "Sélectionnez la juridiction dans le panneau latéral"
+    
     col1, col2, col3 = st.columns([1,1, 3] )
+    
     with col1:
         st.write(df_jur_no_origin.describe()) 
+        
     with col2:    
         vmin = df_jur_no_origin["area"].describe().loc["min"]
         vavg = df_jur_no_origin["area"].describe().loc["mean"]
@@ -179,16 +175,25 @@ with st.container(border=True):
         "Superficie par année"
         st.bar_chart(data = df_jur_no_origin, x = "year", y="area")
 
+
+
     "Histogramme des superficies"
     figy = px.histogram(df_jur, x="area")
     st.plotly_chart(figy, theme="streamlit", use_container_width=False)
 
 
+
+
+############################################
+# origine
+############################################ 
 with st.container(border=True):
+
     subh = "Origine des incendies"  + " (" + jur + ")"
     st.subheader(subh)
     
     col1, col2 = st.columns([1,3] )
+    
     with col1:  
         df_temp = df_jur['origin'].value_counts() #.plot(kind='bar')
         "Nombre d'incendies par origine"
@@ -198,8 +203,10 @@ with st.container(border=True):
         fig = px.histogram(df_jur, x="origin")
         st.plotly_chart(fig, theme="streamlit", use_container_width=False)
 
+
     "Répartitions des origines sur la période couverte"
     st.bar_chart(data = df_jur, x = "year",  y="origin")
+    
     
     "Superficie brulée annuellement par origine"
     scale = "linear"
@@ -220,19 +227,25 @@ with st.container(border=True):
         )
     ).configure_header(labelOrient='bottom', labelPadding = 5).configure_facet(spacing=15)
     st.altair_chart(c, use_container_width=True)         
-        
+
+
     with st.expander("Détails", expanded=False):
+    
         col3, col4 = st.columns([2,4] )
+        
         with col3:     
-            df_jur            
+            df_jur     
+            
         with col4: 
             df1 = pd.pivot_table(df_jur, index=['year'], columns='origin', values='area')
             df1 = df1.reset_index()
             df1
 
 
-    #################
-    
+
+############################################
+# cumul
+############################################   
 with st.container(border=True):    
     st.subheader("Superficie cumulée depuis 1990"  + " (" + jur + ")")
 
@@ -247,9 +260,6 @@ with st.container(border=True):
 
         fig_cumul = px.bar(df_cumul, x="year", y="area")
         st.plotly_chart(fig_cumul, theme="streamlit", use_container_width=False)
-
-#########################################
-
 
 
 
@@ -268,10 +278,8 @@ with st.container(border=True):
     df_jur_sum = df_jur_sum.groupby(["year"]).agg({'area': 'sum'})
 
     df_jur_sum["fft"] = np.fft.fft(df_jur_sum["area"].values)
-
     df_jur_sum = df_jur_sum.reset_index() 
     
-
 
     #########
 
@@ -291,11 +299,11 @@ with st.container(border=True):
     # get the one side frequency
     f_oneside = freq[:n_oneside]
 
-
     # convert frequency to years
     t_h = 1/f_oneside / per
 
     c1, c2 = st.columns(2)
+    
     with c1:
         fig2 = plt.figure(figsize=(6, 3))
         #plt.figure(figsize = (12, 6))
@@ -314,6 +322,7 @@ with st.container(border=True):
         plt.show()
         st.pyplot(fig2)
 
+
     with st.expander("Transformée de Fourier - détails", expanded=False):
         c1, c2 = st.columns(2)
         with c1:
@@ -321,7 +330,9 @@ with st.container(border=True):
         with c2:
             X
             
-            
+
+
+ 
 ############################################
 # footer
 ############################################ 
--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
 # Feux de forêts - Canada - 1990 à 2021
 
-## Application Streamlit pour Analyse exploratoire de données (EDA) des feux de fôrets au Canada entre 1990 et 2023
+## Application Streamlit pour Analyse exploratoire de données (EDA) des feux de fôrets au Canada entre 1990 et 2021
